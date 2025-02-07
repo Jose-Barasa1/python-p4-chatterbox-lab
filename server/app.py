9823f8c8673d09b7d3f8cdbbea3ec0f5a3678929
Jose-Barasa1 <barasajose90@gmail.com>
2025-02-07 09:57:26 +0300

    final commit

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify, abort
 from flask_cors import CORS
 from flask_migrate import Migrate
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import asc
 from models import db, Message
 
 app = Flask(__name__)
@@ -14,13 +15,37 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(asc(Message.created_at)).all()
+    return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    if not data or not 'body' in data or not 'username' in data:
+        abort(400, description="Invalid request data")
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    data = request.get_json()
+    message = Message.query.get_or_404(id)
+    if 'body' in data:
+        message.body = data['body']
+    db.session.commit()
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
+
 
 if __name__ == '__main__':
     app.run(port=5555)
